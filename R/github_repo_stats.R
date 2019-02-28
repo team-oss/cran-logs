b1e1ed1cdf368fe54bd84723a58f7a8ecb4dbc8f
Aaron D. Schroeder <aschroed@virginia.edu>
2019-02-28 18:11:47 +0000

    added error handling to repo_inserts_deletes

--- R/github_repo_stats.R
+++ R/github_repo_stats.R
@@ -4,25 +4,42 @@ repo_inserts_deletes <- function(repos_dir_path = "/mnt/volume_nyc1_01/cloned_re
   library("forcats")
   
   repos <- list.dirs(repos_dir_path, recursive = FALSE)
+  
+  
   if (test_limit != 0)  repos <- repos[1:test_limit]
   repos_cnt <- length(repos)
+  
   print(paste("repos found:", repos_cnt))
   
   i <- 1
   out.matrix <- matrix(NA, nrow = length(repos), ncol = 4)
   colnames(out.matrix) <- c("repo","commit","insertion","deletion")
   
-  for (repo in repos){
-    print(paste("processing repo", i, "of", repos_cnt))
-
-    init_gitsum(repo,over_write = TRUE)
+  for (repo in repos) {
+    print(paste("processing repo", basename(repo), i, "of", repos_cnt))
+    result <- tryCatch({
+      out.matrix[i, 1] <- basename(repo)
+      init_gitsum(repo, over_write = TRUE)
+      log <- parse_log_detailed(repo)
+      out.matrix[i, 2] <- nrow(log)
+      out.matrix[i, 3] <- sum(log$total_insertions)
+      out.matrix[i, 4] <- sum(log$total_deletions)
+      
+    }, warning = function(war) {
+      print(paste("MY_WARNING:  ",war))
+      return(NA)
+      
+    }, error = function(err) {
+      #browser()
+      print(paste("ERROR:  ", err))
+      #o <- out.matrix[i, 1] <- paste(basename(repo), "ERROR")
+      return(NA)
+      
+    }, finally = {
+      
+    })
     
-    log <- parse_log_detailed(repo)
-
-    out.matrix[i,2] <- nrow(log)
-    out.matrix[i,3] <- sum(log$total_insertions)
-    out.matrix[i,4] <- sum(log$total_deletions)
-    i <- i+1
+    i <- i + 1
   }
   out.matrix
 }
