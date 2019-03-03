68b8f38cb49be2c0c5be957de17a811c6e67c76b
Daniel Chen <dc4fj@virginia.edu>
2019-03-03 21:47:40 +0000

    get bugreport link with url to get github links

--- analysis/02-process_scraped/cran/06-parse_github_url.R
+++ analysis/02-process_scraped/cran/06-parse_github_url.R
@@ -5,10 +5,10 @@ library(stringr)
 
 source(here::here('./R/url_to_gh_slug.R'))
 
-get_url_from_meta_tbl <- function(tbl) {
+get_variable_from_meta_tbl <- function(tbl, var) {
   url <- tbl %>%
-    dplyr::filter(X1 == 'URL:') %>%
-    dplyr::pull(X2)
+    dplyr::filter(X1 == var) %>%
+    dplyr::pull(X2)dis
   if (length(url) == 0) {
     return(NA)
   } else {
@@ -18,49 +18,72 @@ get_url_from_meta_tbl <- function(tbl) {
 
 prod_osi <- readr::read_rds(here::here('./data/oss2/processed/cran/production_ready_osi_approved.RDS'))
 
+
+## we found out that some CRAN packages have the github url in the bugreport column,
+# so we will need to parse that: `BugReports:`
+
 with_url <- prod_osi %>% 
-  dplyr::mutate(url = purrr::map_chr(pkg_metadata, get_url_from_meta_tbl))
+  dplyr::mutate(url_section = purrr::map_chr(pkg_metadata, get_variable_from_meta_tbl, var = 'URL:'),
+                bug_section = purrr::map_chr(pkg_metadata, get_variable_from_meta_tbl, var = 'BugReports:'),
+                # separating the 2 vectors with a comma see the multiple_url_parse_gh_slug function for other delimiters
+                url = paste(url_section, bug_section, collapse = ',') 
+                )
 
 with_url$pkg_metadata[[1]]
 with_url$pkg_metadata[[2]]
 with_url$pkg_metadata[[3]]
 
-with_url <- prod_osi %>% 
-  dplyr::mutate(url = purrr::map_chr(pkg_metadata, get_url_from_meta_tbl)) %>%
+with_url <- with_url %>%
+  #dplyr::mutate(url = purrr::map_chr(pkg_metadata, get_variable_from_meta_tbl, var = 'URL:')) %>%
   dplyr::filter(!is.na(url))
 
 gh_url <- with_url %>%
   dplyr::filter(stringr::str_detect(url, 'github'))
 
-gh_url$url
+# gh_url$url
 
 ## findings, some libraries have multiple urls listed: delimiters are , ,\n and maybe \n
 ## some repos have multiple github repos listed
 ## sometimes the seond gh rpo is the devel version
 ## the first url is not necessarily the gh one
 
-
-count_urls <- function(url_string, url_delim_pattern = ',\\n|,|\\n') {
-  # the order of ,\\n and \n or , matter here
-  stringr::str_count(url_string, url_delim_pattern) + 1
-}
-testthat::expect_equal(count_urls('a,b,c'), 3)
-testthat::expect_equal(count_urls('a,\nb,\nc'), 3)
-testthat::expect_equal(count_urls('a\nb\nc'), 3)
-testthat::expect_equal(count_urls('a\nb,c'), 3)
-
-
 gh_url <- gh_url %>%
-  dplyr::mutate(num_urls = purrr::map_dbl(url, count_urls),
+  dplyr::mutate(num_urls = purrr::map_dbl(url, .GlobalEnv$count_urls),
                 gh_slug = purrr::map2_chr(url, num_urls, multiple_url_parse_gh_slug))
 
 
-# only expect 9 values that do not have gh_slug
+# only expect 0 values that do not have gh_slug
 gh_url %>%
   dplyr::select(url, num_urls, gh_slug) %>%
   dplyr::filter(is.na(gh_slug)) %>%
   nrow() %>%
-  testthat::expect_equal(9)
+  testthat::expect_equal(0)
+
+
+gh_url$gh_slug_valid <- stringr::str_detect(gh_url$gh_slug, '.+/.+')
+
+table(gh_url$gh_slug_valid, useNA = 'always')
+
+gh_url %>%
+  dplyr::filter(gh_slug_valid == FALSE) %>%
+  dplyr::select(pkg_name, url, gh_slug, gh_slug_valid) %>%
+  print(n = 1000)
+
+## findings: all the invalid slugs are just user/ so we can append the pkg_name to get a valid slug
+
+gh_url <- gh_url %>%
+  dplyr::mutate(
+    gh_slug = dplyr::case_when(
+      gh_slug_valid == FALSE ~ stringr::str_c(gh_slug, pkg_name),
+      gh_slug_valid == TRUE ~ gh_slug
+    )
+  )
+
+gh_url %>%
+  dplyr::filter(gh_slug_valid == FALSE) %>%
+  dplyr::select(pkg_name, url, gh_slug, gh_slug_valid) %>%
+  print(n = 1000)
+
 
 gh_url$gh_clone_url <- paste0('https://github.com/', gh_url$gh_slug, '.git')
 gh_url
@@ -72,6 +95,7 @@ gh_url %>%
   dplyr::filter(production_ready == TRUE,
                 osi_approved == TRUE,
                 !is.na(gh_slug)) %>%
-  nrow() ## 3888
+  nrow() ## 13143
+
 
 readr::write_rds(gh_url, here::here('./data/oss2/processed/cran/production_osi_gh.RDS'))
--- analysis/03-github/01-03-clone_cran.R
+++ analysis/03-github/01-03-clone_cran.R
@@ -1,34 +1,149 @@
+#
+#
+# useage Rscript 01-03-clone_cran.R
+# useage Rscript 01-03-clone_cran.R 5 1
+# useage Rscript 01-03-clone_cran.R 5 4
+# useage Rscript 01-03-clone_cran.R 5 0
+# useage Rscript 01-03-clone_cran.R r
+
 library(readr)
 library(here)
 library(git2r)
 library(dplyr)
 
-cran <- readr::read_rds(here::here('./data/oss2/processed/cran/production_osi_gh.RDS'))
+# capture cmd args
 
-cran$gh_slug_valid <- stringr::str_detect(cran$gh_slug, '.+/.+')
+args <- commandArgs(trailingOnly = TRUE)
 
-table(cran$gh_slug_valid, useNA = 'always')
+print(length(args))
+print(args)
 
-cran %>%
-  dplyr::filter(gh_slug_valid == FALSE) %>%
-  dplyr::select(pkg_name, url, gh_slug, gh_slug_valid) %>%
-  print(n = 1000)
+num_args <- length(args)
+if (num_args == 1) {
+  # if you only pass in 1 parameter, I am expecting r as the value
+  stopifnot(any(args %in% c('r')))
+  mod <- NULL
+  part <- args
+} else if (num_args == 2) {
+  mod <- as.integer(args[[1]])
+  part <- as.integer(args[[2]])
+  if (mod == part) {
+    stop('the mod and part are the same value')
+  }
+} else if (num_args == 0) {
+  # nothing passed, so just work forward in sequence
+  mod <- NULL
+  part <- 'f'
+} else {
+  stop('Unknown number of argements passed')
+}
+
+# script -----
+
+cran <- readr::read_rds(here::here('./data/oss2/processed/cran/production_osi_gh.RDS')) %>%
+  dplyr::arrange(pkg_name)
+
+## this vector was created by running this script and checking the URLs that failed
+no_work <- c(
+  # bad or unparsed URLs so the slug was just NA
+  "https://github.com/NA.git",
+  
+  # repo does not exist
+  "https://github.com/BacArena/BacArena.git",
+  "https://github.com/braithwaite/BIOM.utils.git",
+  "https://github.com/divadnojnarg/bs4Dashindex.html.git",
+  "https://github.com/alecri/softwaredosresmeta.html.git",
+  "https://github.com/jinghuazhao/gap.datasets.git",
+  "https://github.com/stlane/gimmeTools.git",
+  "https://github.com/wencke/wenckehub.io.git",
+  "https://github.com/magnusmunch/gren.git",
+  "https://github.com/alecri/software.git",
+  "https://github.com/zedoul/jsonstat.git",
+  "https://github.com/aaronolsen/software.git",
+  "https://github.com/derek-corcoran-barrios/VignetteNetworkExt.html.git",
+  "https://github.com/ramey/noncensus.git",
+  "https://github.com/rstudio/radix.git",
+  "https://github.com/datastorm-open/introduction_ramcharts.git", # maybe renamed to rAmCharts? there are other ramcharts
+  "https://github.com/thirdwing/RcppDL.git",
+  "https://github.com/derek-corcoran-barrios/SpatialBall.html.git",
+  "https://github.com/drastega/docs.git",
+  "https://github.com/aaronolsen/tutorials.git",
+  "https://github.com/petolau/package.git",
+  "https://github.com/mststats/#software.git", # link to software page on website
+  
+  # user does not even exist
+  "https://github.com/jmackie4/cycleRtools.git"
+)
+
+if (part == 'r') {
+  # if r is passed, reverse the dataframe
+  cran <- purrr::map_df(cran, rev)
+}
 
 for (i in seq_along(cran$pkg_name)) {
-  clone_url <- cran$gh_clone_url[[i]]
-  clone_pth <- cran$gh_clone_path[[i]]
-  clone_pth <- here::here(clone_pth)
-  
-  if (dir.exists(clone_pth)) {
-    next
-  } else {
-    print(i)
-    print(clone_url)
-    print(clone_pth)
-    git2r::clone(url = clone_url, local_path = clone_pth, progress = TRUE)
+  if (is.null(mod)) { # either going forward or backward
+    
+    ## begin copy ##
+    clone_url <- cran$gh_clone_url[[i]]
+    clone_pth <- cran$gh_clone_path[[i]]
+    clone_pth <- here::here(clone_pth)
+    # skip these, they're 404s
+    if (clone_url %in% no_work) {
+      print("SKIP")
+      print(i)
+      print(clone_url)
+      #Sys.sleep(1) ## helps you see it
+      next
+    }
+    
+    if (dir.exists(clone_pth)) {
+      next
+    } else {
+      print(i)
+      print(clone_url)
+      print(clone_pth)
+      try({
+        git2r::clone(url = clone_url, local_path = clone_pth, progress = TRUE)
+      }, outFile = './clone_cran_error.txt')
+    }
+    # break
+    ## end copy ##
+
+  } else { # doing a modulo split
+    mod_res <- i %% mod
+    if (mod_res == part) {
+      
+      ## begin copy ##
+      clone_url <- cran$gh_clone_url[[i]]
+      clone_pth <- cran$gh_clone_path[[i]]
+      clone_pth <- here::here(clone_pth)
+      # skip these, they're 404s
+      if (clone_url %in% no_work) {
+        print("SKIP")
+        print(i)
+        print(clone_url)
+        #Sys.sleep(1) ## helps you see it
+        next
+      }
+      
+      if (dir.exists(clone_pth)) {
+        next
+      } else {
+        print(i)
+        print(clone_url)
+        print(clone_pth)
+        try({
+          git2r::clone(url = clone_url, local_path = clone_pth, progress = TRUE)
+        }, outFile = './clone_cran_error.txt')
+      }
+      # break
+      ## end copy ##
+      
+    } else {
+      next
+    }
   }
-  # break
 }
 
 
-cran[44, ]
+# cran[668, ] %>% dplyr::select(pkg_name, url, gh_slug, gh_clone_url)
